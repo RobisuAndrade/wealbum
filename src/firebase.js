9c9a7cb341803efbf0b8768e3cd5c33558f5b6cd
RobisuAndrade <robsonfreitas1999@gmail.com>
2026-05-27 01:51:54 -0300

    Atualização da regra do banco de dados

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore"; // Adicionado para o Banco de Dados
+import { getFirestore } from "firebase/firestore"; // IMPORTANTE: Adicione esta linha
 
-// As credenciais do seu projeto WE-ALBUM
 const firebaseConfig = {
   apiKey: "AIzaSyAdcOG88cJW3DO5OtjOzKrrEJMjRg4vMWs",
   authDomain: "we-album.firebaseapp.com",
+  databaseURL: "https://we-album-default-rtdb.firebaseio.com",
   projectId: "we-album",
   storageBucket: "we-album.firebasestorage.app",
   messagingSenderId: "396719409653",
@@ -14,11 +14,8 @@ const firebaseConfig = {
   measurementId: "G-52J65SHWJ1"
 };
 
-// Inicializando o Firebase
 const app = initializeApp(firebaseConfig);
-
-// Inicializando o Analytics (opcional, bom ter)
 const analytics = getAnalytics(app);
 
-// Inicializando o Banco de Dados (Firestore) e exportando para usarmos no app
+// EXPORTANDO O BANCO DE DADOS
 export const db = getFirestore(app);
